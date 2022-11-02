c0b0b7bbc75876b254d9ef0105cfd24aec8ba9e8
StefanRieger <riste2@gmx.de>
2022-11-02 10:31:30 +0100

    Initial Commit

---
+++ GitHubDemo/GitHubDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHubDemoApp.swift
+//  GitHubDemo
+//
+//  Created by Stefan Rieger on 02.11.22.
+//
+
+import SwiftUI
+
+@main
+struct GitHubDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
